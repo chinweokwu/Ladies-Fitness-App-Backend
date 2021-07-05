4b99449a9a2c1fcd0aff35afb2148eb9fbd4a8c2
chinweokwu <paulcmorah@gmail.com>
2021-07-05 09:53:38 +0100

    add heroku link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://localhost:3001'
+     origins 'https://serene-beyond-13704.herokuapp.com/'
 
      resource '*',
        headers: :any,
