c31db71344554790773a84296e464a741b45c013
chinweokwu <paulcmorah@gmail.com>
2021-09-07 05:31:31 +0100

    change auth error name

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,7 +1,6 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
-  # Define custom error subclasses - rescue catches `StandardErrors`
   class AuthenticationError < StandardError; end
 
   class MissingToken < StandardError; end
@@ -12,8 +11,8 @@ module ExceptionHandler
     # Define custom handlers
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::MissingToken, with: :bad_request
+    rescue_from ExceptionHandler::InvalidToken, with: :unauthorized_request
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -22,12 +21,14 @@ module ExceptionHandler
 
   private
 
-  # JSON response with message; Status code 422 - unprocessable entity
-  def four_twenty_two(error)
-    json_response({ message: error.message }, :unprocessable_entity)
+  def bad_request(error)
+    json_response({ message: error.message }, :bad_request)
   end
 
-  # JSON response with message; Status code 401 - Unauthorized
+  def four_twenty_two(error)
+    json_response({ message: error.message }, :unprocessable entity)
+  end
+
   def unauthorized_request(error)
     json_response({ message: error.message }, :unauthorized)
   end
